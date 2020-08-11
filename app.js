19abcd23e96e9215cb7d513a5055c88af7925384
VilliamVIX <1059448504@qq.com>
2020-08-11 10:34:25 +0800

    fi

---
+++ app.js
@@ -0,0 +1,62 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+global.baseURL="http://192.168.1.100:3008"
+global.lenURL=25
+
+app.set('view engine', 'html');
+app.engine('html', require('express-art-template'));
+app.all("*", function(req, res, next) {
+	var orginList = [
+		"http://192.168.1.100",
+		'http://59.61.83.130:29080',
+	]
+	if (orginList.includes(req.headers.origin)) {
+		//设置允许跨域的域名，*代表允许任意域名跨域
+		res.header("Access-Control-Allow-Origin", req.headers.origin);
+	}
+	// res.clearCookie('id')
+	// res.cookie(prop, '', {expires: new Date(0)});
+	res.set("Access-Control-Allow-Methods", "POST,GET,DELETE,PUT");
+	res.set("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+	res.set("Access-Control-Allow-Credentials", true);
+	// res.set('Access-Control-Allow-Max-Age', 3600);
+	if ("OPTIONS" === req.method) return res.sendStatus(200);
+	next();
+});
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  // res.render('error');
+});
+
+console.log('分析/同步服务启动，端口：3009')
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,134 @@
+const express = require('express'),
+	router = express.Router(),
+
+	fs = require('fs'),
+
+	//获得WebSocketServerr类型
+	WebSocketServer = require('ws').Server,
+	//创建WebSocketServer对象实例，监听指定端口
+	wss = new WebSocketServer({
+		port: 3010
+	}),
+	wssCAN_PENG = new WebSocketServer({
+		port: 3011
+	}),
+	wssYI_HUI = new WebSocketServer({
+		port: 3012
+	}),
+	wssGENG_DONG = new WebSocketServer({
+		port: 3013
+	}),
+	wssBAI = new WebSocketServer({
+		port: 3014
+	}),
+
+	//创建保存所有已连接到服务器的客户端对象的数组
+	clients = []
+
+
+
+// 数据展示页面
+router.get('/chart', (req, res) => {
+	res.render('chart.html', {})
+});
+
+// ws接受消息頁面
+router.get('/get/wsmsg', (req, res) => {
+	console.log('connect analysis')
+	res.render('getMsg.html', {})
+});
+
+
+// 查询页面基础路径
+router.get('/get/baseurl', (req, res) => {
+	console.log(global.baseURL)
+	const data = {
+		baseURL: global.baseURL
+	}
+	res.json(data)
+});
+
+// 查询页面基础路径
+router.post('/smqform', (req, res) => {
+
+	const data =`
+	
+	`
+		
+
+	// res.json({
+		// html:data
+	// })
+	res.send(data)
+});
+
+
+
+//为服务器添加connection事件监听，当有客户端连接到服务端时，立刻将客户端对象保存进数组中
+wss.on('connection', function(client) {
+	// console.log(client)
+	console.log("一个客户端连接到服务器")
+	if (clients.indexOf(client) === -1) { //如果是首次连接
+		clients.push(client) //就将当前连接保存到数组备用
+
+		console.log("有" + clients.length + "客户端在线")
+		//为每个client对象绑定message事件，当某个客户端发来消息时，自动触发
+		client.on('message', function(msg) {
+
+			console.log('收到消息' + msg)
+			//遍历clients数组中每个其他客户端对象，并发送消息给其他客户端
+			for (var c of clients) {
+				if (c != client) { //把消息发给别人
+					c.send(msg);
+				}
+			}
+		})
+
+	}
+})
+
+wssCAN_PENG.on('connection', function(client) {
+	// console.log(client)
+	console.log("一个客户端连接到服务器")
+	if (clients.indexOf(client) === -1) { //如果是首次连接
+		clients.push(client) //就将当前连接保存到数组备用
+
+		console.log("有" + clients.length + "客户端在线")
+		//为每个client对象绑定message事件，当某个客户端发来消息时，自动触发
+		client.on('message', function(msg) {
+
+			console.log('收到消息' + msg)
+			//遍历clients数组中每个其他客户端对象，并发送消息给其他客户端
+			for (var c of clients) {
+				if (c != client) { //把消息发给别人
+					c.send(msg);
+				}
+			}
+		})
+
+	}
+})
+
+wssGENG_DONG.on('connection', function(client) {
+	// console.log(client)
+	console.log("一个客户端连接到服务器")
+	if (clients.indexOf(client) === -1) { //如果是首次连接
+		clients.push(client) //就将当前连接保存到数组备用
+
+		console.log("有" + clients.length + "客户端在线")
+		//为每个client对象绑定message事件，当某个客户端发来消息时，自动触发
+		client.on('message', function(msg) {
+
+			console.log('收到消息' + msg)
+			//遍历clients数组中每个其他客户端对象，并发送消息给其他客户端
+			for (var c of clients) {
+				if (c != client) { //把消息发给别人
+					c.send(msg);
+				}
+			}
+		})
+
+	}
+})
+
+module.exports = router;
